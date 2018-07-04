f2b593e06be27e65dcd093a2d177fd788aad786e
Richie Loughran <richie.loughran@ericsson.com>
2018-07-04 12:26:23 +0100

    Added Start Scripts & Databse Loader Scripts

---
+++ postgres_files/create_db.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+source common_library
+
+#**************************************************************************
+# This function creates the <vxfs_test_db> db, if database doesn't exists
+#**************************************************************************
+create_db()
+{
+    ${_ECHO} " " > ${LOG_FILE}
+    infoLog "Dropping Test database if it EXISTS : ${DB}"
+    run_psql_query '"DROP DATABASE IF EXISTS '${DB}';"' >> ${LOG_FILE}
+
+    infoLog "Attempting to create ${DB} database"
+    run_psql_query '"CREATE DATABASE '"\"'${DB}'"\"' WITH OWNER ='"\"'${PG_USER}'"\"' ENCODING ='\''UTF8'\''
+    TABLESPACE= pg_default LC_COLLATE ='\''en_US.UTF-8'\'' LC_CTYPE ='\''en_US.UTF-8'\''
+    CONNECTION LIMIT =-1;"' >> ${LOG_FILE}
+
+    checkExitCode "Creating database ${DB}"
+}
+
+create_table()
+{
+    infoLog "Attempting to connect to ${DB}"
+    run_psql_query  '"\c '${DB}'"' >> ${LOG_FILE}
+
+    infoLog "Attempting to create Table: ${1}"
+    run_psql_query_on_db ${DB} '"CREATE TABLE IF NOT EXISTS '"\"'${1}'"\"'
+    ( id int, md5Number char(40)); "' >> ${LOG_FILE}
+
+    checkExitCode "Creating Table ${1}"
+}
+
+
+create_db
+create_table test_table_1
+create_table test_table_2
+load_data test_table_1 20
+source info_db.sh
