4d3c9b13d26ed3a2e10cc68de40227e56408d3b0
Richie Loughran <richie.loughran@ericsson.com>
2018-07-11 17:42:16 +0100

    Added recovery.conf and still need script for backup

--- follower/postinstall_files/generate_load.sh
+++ follower/postinstall_files/generate_load.sh
@@ -8,7 +8,7 @@ function apply_load()
 {
     for ((i=0; i<=$COUNTER; i++));
     do
-        load_data test_table_2 100000
+        load_data test_table_2 1000
         infoLog "Row List AFTER Applying Load Number #$i"
         get_row_list
     done
--- leader/postinstall_files/start_postgres_leader.sh
+++ leader/postinstall_files/start_postgres_leader.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
 _CAT=/bin/cat
+_CHOWN=/bin/chown
 _ECHO=/bin/echo
 _SERVICE="/sbin/service"
 _UNIX_LOGGER="/usr/bin/logger"
 
-APP_ID="PG_HA_FOLLOWER"
-FOLLOWER_DIR=/postgres_follower
-NEW_CONF=${FOLLOWER_DIR}/follower_postgres.conf
-NEW_HBA=${FOLLOWER_DIR}/follower_hba.conf
+APP_ID="PG_HA_LEADER"
+LEADER_DIR=/postgres_leader
+NEW_CONF=${LEADER_DIR}/leader_postgres.conf
+NEW_HBA=${LEADER_DIR}/leader_hba.conf
 PGCONF=/var/lib/pgsql/9.6/data/postgresql.conf
 PGDATA=/var/lib/pgsql/9.6/data/
 PGHBA=/var/lib/pgsql/9.6/data/pg_hba.conf
 PG_SERVICE=postgresql-9.6
+SHARED_FS_WALS="/shared_wals"
+SHARED_FS_BACKUP="/backup_dir"
 
 
 logger()
@@ -39,7 +42,7 @@ update_conf_files()
     logger info "Attempting to append ${PGCONF} with ${NEW_CONF}"
     ${_CAT} ${NEW_CONF} >> ${PGCONF}
     RETVAL=$?
-    if [[ ${RETVAL} -ne 0 ]] ;
+    if [[ ${RETVAL} -ne 0 ]];
     then
         logger error "Failed to append ${PGCONF} with ${NEW_CONF}"
     else
@@ -49,7 +52,7 @@ update_conf_files()
     logger info "Attempting to replace ${PGHBA} with ${NEW_HBA}"
     ${_CAT} ${NEW_HBA} > ${PGHBA}
     RETVAL=$?
-    if [[ ${RETVAL} -ne 0 ]] ;
+    if [[ ${RETVAL} -ne 0 ]];
     then
         logger error "Failed to replace ${PGHBA} with ${NEW_HBA}"
     else
@@ -61,15 +64,13 @@ initialise_db()
 {
     logger info "${PGDATA} is empty...Initialising Database"
     logger info "Attempting to initdb the service PostgreSQL"
-    ${_SERVICE} ${PG_SERVICE} initdb >/dev/null 2>&1
-    RETVAL=$?
-    if [[ ${RETVAL} -ne 0 ]];
+    retval=$(${_SERVICE} ${PG_SERVICE} initdb)
+    if [[ ${retval} = *"FAILED"* ]];
     then
         logger error "Failed to initialise PostgreSQL service"
     else
         logger info "PostgreSQL service was initialised"
     fi
-
     update_conf_files
 }
 
@@ -82,7 +83,7 @@ start_postgres()
     logger info "Attempting to START the service PostgreSQL"
     ${_SERVICE} ${PG_SERVICE} start >/dev/null 2>&1
     RETVAL=$?
-    if [[ ${RETVAL} -ne 0 ]] ;
+    if [[ ${RETVAL} -ne 0 ]];
     then
         logger error "Failed to start PostgreSQL service"
         logger debug "Investigate the log file:: cat /var/lib/pgsql/9.6/pgstartup.log"
@@ -97,17 +98,41 @@ start_rsyslog()
     logger info "Attempting to START the service rsyslog"
     ${_SERVICE} rsyslog start
     RETVAL=$?
-    if [[ ${RETVAL} -ne 0 ]] ;
+    if [[ ${RETVAL} -ne 0 ]];
     then
         logger error "Failed to start rsyslog service"
     else
         ${_ECHO} "" > /var/log/messages
-        logger info "rsyslog service was started."
+        logger info "rsyslog service was started"
         logger debug "Log messages can now be viewed in /var/log/messages"
 
     fi
 }
 
+change_ownership_sharedfs()
+{
+    logger info "Attempting to change ownership of ${SHARED_FS_WALS} directory to postgres"
+    ${_CHOWN} postgres:postgres ${SHARED_FS_WALS}
+    RETVAL=$?
+    if [[ ${RETVAL} -ne 0 ]];
+    then
+        logger error "Failed to change ownership of ${SHARED_FS_WALS} directory to postgres"
+    else
+        logger info "Successfully changed ownership of ${SHARED_FS_WALS} directory to postgres"
+    fi
+
+    ${_CHOWN} postgres:postgres ${SHARED_FS_BACKUP}
+    RETVAL=$?
+    if [[ ${RETVAL} -ne 0 ]];
+    then
+        logger error "Failed to change ownership of ${SHARED_FS_BACKUP} directory to postgres"
+    else
+        logger info "Successfully changed ownership of ${SHARED_FS_BACKUP} directory to postgres"
+    fi
+}
+
 #MAIN
 start_rsyslog
+change_ownership_sharedfs
 start_postgres
+source create_replicator_role.sh
