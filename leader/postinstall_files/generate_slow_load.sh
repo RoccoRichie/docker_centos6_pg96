e82337b861c4ed7b7ca24f341358dd204d947c3c
Richie Loughran <richie.loughran@ericsson.com>
2018-07-16 14:12:15 +0100

    Added a slow loader

---
+++ leader/postinstall_files/generate_slow_load.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+source common_library
+
+_SLEEP=/bin/sleep
+
+COUNTER=100
+
+function apply_load()
+{
+    for ((i=0; i<=$COUNTER; i++));
+    do
+        load_data test_table_2 1000
+        ${_SLEEP} 1
+        infoLog "Row List AFTER Applying Load Number #$i"
+        get_row_list
+    done
+}
+
+while getopts c: option
+do
+   case "${option}"
+   in
+       c) COUNTER=${OPTARG};;
+   esac
+done
+
+infoLog "Row List BEFORE Applying Load"
+get_row_list
+time apply_load
+sleep 1
+
+source info_db.sh
